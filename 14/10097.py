963b16990b5fff0146f455c11383c18fd2b2f149
Kosuma <59332196+GRom6@users.noreply.github.com>
2023-11-22 17:40:21 +0300

    fix 10097.py

--- 14/10097.py
+++ 14/10097.py
@@ -3,7 +3,7 @@ def convert(n):
   while n:
     res.append(n%25)
     n //= 25
-  return res[::-2]
+  return res[::-1]
 
 number = (3 * 3125**8 + 2*625**7 - 4 * 625**6 + 3 * 125**5 - 2 * 25**4 - 2024)
 n = counter(number)
