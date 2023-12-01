e31b4767ae49db79056210021a02d62d693c4902
Gorelikov Roman <romka.gorelikov@gmail.com>
2023-12-01 16:12:33 +0300

    lesson 01.12.23
    5 ex

--- 25/4202Pol.py
+++ 25/4202Pol.py
@@ -1,17 +1,16 @@
-def prod(n):
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+def prod(number):
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
             return False
     else:
         return True
-prost =  []
-for i in range(3000000, 10000000):
-    if prod(i):
-        prost.append(i)
+
+
+prost = [x for x in range(3*10**6, 10*10**6) if prod(x)]
 count_pare = 0
 avg_last_pare = 0
-for i in range(len(prost)):
-    if prost[i] - prost[i-1] == 2:
+for i in range(1, len(prost)):
+    if prost[i] - prost[i - 1] == 2:
         count_pare += 1
         avg_last_pare = prost[i] - 1
 print(count_pare, avg_last_pare)
